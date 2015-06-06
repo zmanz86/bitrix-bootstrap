0202d38510f692d9b719eab9ded1fa3d05ac3b5d
Ganin Aleksey <zmanz@yandex.ru>
2015-06-06 13:58:03 +0500

    new_bitrix_template

--- footer.php
+++ footer.php
@@ -1,41 +1,7 @@
-<?if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();
-IncludeTemplateLangFile(__FILE__);
-?>
-			</div>
-		</div>
-		<div id="space-for-footer"></div>
-	</div>
-	
-	<div id="footer">
-	
-		<div id="copyright">
-<?
-$APPLICATION->IncludeFile(
-	SITE_DIR."include/copyright.php",
-	Array(),
-	Array("MODE"=>"html")
-);
-?>
-		</div>
-		<div class="footer-links">	
-<?
-$APPLICATION->IncludeComponent("bitrix:menu", "bottom", array(
-	"ROOT_MENU_TYPE" => "bottom",
-	"MENU_CACHE_TYPE" => "N",
-	"MENU_CACHE_TIME" => "36000000",
-	"MENU_CACHE_USE_GROUPS" => "Y",
-	"MENU_CACHE_GET_VARS" => array(
-	),
-	"MAX_LEVEL" => "1",
-	"CHILD_MENU_TYPE" => "left",
-	"USE_EXT" => "N",
-	"ALLOW_MULTI_SELECT" => "N"
-	),
-	false
-);
-?>
-		</div>
-		<div id="footer-design"><?=GetMessage("FOOTER_DISIGN")?></div>
-	</div>
+<?if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();?>
+	<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="js/bootstrap.js"></script>
 </body>
 </html>
